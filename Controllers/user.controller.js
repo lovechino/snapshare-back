25ef6338f529483144fe35f11ebce0cd9a165f2e
lovechino <quochung13122002@gmail.com>
2025-04-14 22:46:35 +0700

    c

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -80,7 +80,8 @@ const Login = async(req,res)=>{
             following : getUser.following
         }
         return res.cookie('token',token,
-            { httpOnly: true, 
+            { 
+            //  httpOnly: true, 
               sameSite: 'lax', 
               maxAge: 1 * 24 * 60 * 60 * 1000,
               secure: true,
