6ce03cb5ef715184c6960157722a69a1201db24c
lovechino <quochung13122002@gmail.com>
2025-04-14 20:48:01 +0700

    test

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ dotenv.config()
 
 app.use(express.json())
 app.use(cookieParser())
+app.set("trust proxy", 1);
 app.use(cors({
     origin :'https://snapshare-front.onrender.com',
      credentials: true
