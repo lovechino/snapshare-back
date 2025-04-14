97ba3761af56c67d6d15e19dbb5f9b585c7dd632
lovechino <quochung13122002@gmail.com>
2025-04-14 20:41:28 +0700

    c

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     origin :'https://snapshare-front.onrender.com',
-     credentials: true,
- 
+     credentials: true
     }))
 app.use(express.urlencoded({extended:true}))
 
